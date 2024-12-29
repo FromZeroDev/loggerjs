8fba91c4c8332522bb16622225c0fa88fea2f7f2
ross96D <abelaross96@gmail.com>
2024-12-29 15:44:07 -0500

    change import in output_file to es module

--- src/output_file.js
+++ src/output_file.js
@@ -1,14 +1,15 @@
-const fs = require("fs");
+import {createWriteStream, WriteStream} from "node:fs";
+
 
 export class WriteStreamOutput {
     stream
 
     constructor (file) {
-        if (file instanceof fs.WriteStream) {
+        if (file instanceof WriteStream) {
             this.stream = file;
         } else if (file instanceof String || typeof file === "string") {
             const path = file;
-            this.stream = fs.createWriteStream(path.toString())
+            this.stream = createWriteStream(path.toString())
         } else {
             throw new Error("unsupported argument of type " + typeof file + " expected Node fs.WriteStream or a path")
         }
